8e60bbb99168b7c9a7756976b6829e755c785d0c
León Felipe Guevara Chávez <leon.guevara@tec.mx>
2026-01-06 11:25:38 -0600

    Initial Commit

---
+++ MyAccountingBooks/MyAccountingBooksApp.swift
@@ -0,0 +1,21 @@
+//
+//  MyAccountingBooksApp.swift
+//  MyAccountingBooks
+//
+//  Created by León Felipe Guevara Chávez on 2026-01-06.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct MyAccountingBooksApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ MyAccountingBooksTests/MyAccountingBooksTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyAccountingBooksTests.swift
+//  MyAccountingBooksTests
+//
+//  Created by León Felipe Guevara Chávez on 2026-01-06.
+//
+
+import Testing
+@testable import MyAccountingBooks
+
+struct MyAccountingBooksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
